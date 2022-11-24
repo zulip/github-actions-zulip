161f59837972150ce554ec3f0dae571788e59da9
Josh Klar <josh@zulip.com>
2022-11-23 19:41:14 -0800

    deps: Install and configure ESLint based on XO defaults.

--- src/send-message.js
+++ src/send-message.js
@@ -1,73 +1,78 @@
-import { getInput, info, setFailed } from '@actions/core'
-import zulipInit from 'zulip-js'
+import { getInput, info, setFailed } from "@actions/core";
+import zulipInit from "zulip-js";
 
-const allNumericRegex = /^[0-9]+$/
+const allNumericRegex = /^\d+$/;
 
-async function run () {
-  const email = getInput('email', { required: true })
-  const apiKey = getInput('api-key', { required: true })
-  const realm = getInput('organization-url', { required: true })
-  let to = getInput('to', { required: true })
-  const type = getInput('type', { required: true })
-  const topic = getInput('topic', { required: false })
-  const content = getInput('content', { required: true })
+async function run() {
+  const email = getInput("email", { required: true });
+  const apiKey = getInput("api-key", { required: true });
+  const realm = getInput("organization-url", { required: true });
+  let to = getInput("to", { required: true });
+  const type = getInput("type", { required: true });
+  const topic = getInput("topic", { required: false });
+  const content = getInput("content", { required: true });
   const initialConfig = {
     username: email,
-    realm
-  }
-  const config = {}
+    realm,
+  };
+  const config = {};
   if (apiKey) {
-    Object.assign(config, initialConfig, { apiKey })
+    Object.assign(config, initialConfig, { apiKey });
   } else {
-    setFailed('api-key is mandatory in order to interact with the Zulip API.')
-    return
+    setFailed("api-key is mandatory in order to interact with the Zulip API.");
+    return;
   }
-  if (type === 'private') {
+
+  if (type === "private") {
     if (to) {
-      to = to.split(',')
-      const containsUserIds = to.every((item) => item.match(allNumericRegex))
+      to = to.split(",");
+      const containsUserIds = to.every((item) => item.match(allNumericRegex));
       if (containsUserIds) {
-        to = to.map((item) => parseInt(item))
+        to = to.map((item) => Number.parseInt(item));
       }
     }
-  } else if (type === 'stream') {
+  } else if (type === "stream") {
     if (!topic) {
-      setFailed('topic is mandatory when type is "stream".')
-      return
+      setFailed('topic is mandatory when type is "stream".');
+      return;
     }
-    if (to.match(allNumericRegex)) {
-      to = parseInt(to)
+
+    if (allNumericRegex.test(to)) {
+      to = Number.parseInt(to);
     }
   } else {
-    setFailed('type must be one of: private, stream.')
-    return
+    setFailed("type must be one of: private, stream.");
+    return;
   }
+
   try {
-    const client = await zulipInit(config)
+    const client = await zulipInit(config);
     // Send a message
-    const params = {
+    const parameters = {
       to,
       type,
       topic,
-      content
-    }
-    const response = await client.messages.send(params)
-    if (response.result === 'success') {
+      content,
+    };
+    const response = await client.messages.send(parameters);
+    if (response.result === "success") {
       // OK!
-      info(`Message successfully send with id: ${response.id}`)
+      info(`Message successfully send with id: ${response.id}`);
     } else {
-      const errorMessage = response.code ? `${response.code}: ${response.msg}` : response.msg
-      setFailed(new Error(errorMessage))
+      const errorMessage = response.code
+        ? `${response.code}: ${response.msg}`
+        : response.msg;
+      setFailed(new Error(errorMessage));
     }
   } catch (error) {
-    setFailed(error)
+    setFailed(error);
   }
 }
 
-;(async () => {
+(async () => {
   try {
-    await run()
+    await run();
   } catch (error) {
-    setFailed(error)
+    setFailed(error);
   }
-})()
+})();
