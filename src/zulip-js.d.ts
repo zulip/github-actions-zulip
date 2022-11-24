c09b4a0a8f001630a8bcf663445100d946b0e330
Josh Klar <josh@zulip.com>
2022-11-23 19:42:52 -0800

    types: Provide basic structural type hints for zulip-js.

---
+++ src/zulip-js.d.ts
@@ -0,0 +1,42 @@
+// This is currently held in this repo as a testbed, but it should be migrated
+// to the zulip-js repo for broader use and tagged in a version bump at some
+// point soon.
+declare module "zulip-js" {
+  export type ClientConfiguration =
+    | { zuliprc: string }
+    | {
+        realm: string;
+        username: string;
+        apiKey?: string;
+      };
+
+  export type ApiParameters = Record<string, any>;
+  // This type can be narrowed once this file lives in zulip-js appropriately,
+  // as Language Server Protocol is indicating hover-for-type support in that
+  // repo on Response objects, including fields like json() and status.
+  export type UntypedApiCall = (_: ApiParameters) => Promise<any>;
+
+  export type Client = {
+    messages: ClientMessages;
+  };
+
+  export type ClientMessages = {
+    deleteById: UntypedApiCall;
+    deleteReactionById: UntypedApiCall;
+    flags: ClientMessagesFlags;
+    getById: UntypedApiCall;
+    getHistoryById: UntypedApiCall;
+    render: UntypedApiCall;
+    retrieve: UntypedApiCall;
+    send: UntypedApiCall;
+    update: UntypedApiCall;
+  };
+
+  export type ClientMessagesFlags = {
+    add: UntypedApiCall;
+    remove: UntypedApiCall;
+  };
+
+  async function zulipInit(arguments_: ClientConfiguration): Promise<Client>;
+  export default zulipInit;
+}
